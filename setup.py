6beee960f810b564b03922568a88c408578adfa1
ryanpdx <rmedick@pdx.edu>
2020-07-01 18:17:55 -0700

    Converted project into a standard python package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="oresat-linux-updaterd",
+    version="0.0.1",
+    author="Ryan Medocl",
+    author_email="rmedick@pdx.edu",
+    description="A daemon for all oresat linux boards to handle updating the board.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/oresat/oresat-linux-updater",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPLv3 License",
+    ],
+    python_requires='>=3.7',
+)
+
