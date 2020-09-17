fdd138a6f665dbfea2b077203a89e9158cb9db38
ryanpdx <rmedick@pdx.edu>
2020-09-16 23:27:28 -0700

    updated setuptools configs

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="oresat-linux-updaterd",
-    version="0.0.1",
-    author="Ryan Medick",
-    author_email="rmedick@pdx.edu",
-    description="A daemon for all oresat linux boards to handle updating the board.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/oresat/oresat-linux-updater",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPLv3 License",
-    ],
-    python_requires='>=3.7',
-)
-
+setuptools.setup()
